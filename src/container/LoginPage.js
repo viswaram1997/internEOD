366bbc045183c425c78e0578bdf82dd0f9675654
tjsudarsan <tjsudarsan@gmail.com>
2017-12-21 10:14:23 +0530

    ui changes

--- src/container/LoginPage.js
+++ src/container/LoginPage.js
@@ -1,12 +1,12 @@
 import { connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Login from "../component/login/login";
+import LoginPage from "../component/LoginPage";
 import { LoginUser ,SignUp} from "../redux/action/actions";
+
 const mapStateToProps = (state) => {
     return ({
         Login:state.StoreData.LoginStatus,
         SignUp:state.StoreData.SignUp
-
     });
 }
 
@@ -14,10 +14,7 @@ const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
        "LoginUser":LoginUser,
        "SignUp":SignUp
-         
-       
-        
     },dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
