35e08d3216582c99b41992ec16317aaddacc76ec
tjsudarsan <tjsudarsan@gmail.com>
2017-12-22 10:02:35 +0530

    forgotpassword

--- src/component/LoginRegister.js
+++ src/component/LoginRegister.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-import {Grid, Icon, Form, Card, Button} from 'semantic-ui-react'
+import {Grid, Icon, Form, Card, Button, Message} from 'semantic-ui-react'
 
 export default class LoginRegister extends Component {
     constructor(){
         super();
         this.state = {
-            loginRegisterToggle: true,
+            option: 'login',
             registerPage1: true,
-            registerPage2: false
+            registerPage2: false,
+            forgotPage1 : true,
+            forgotPage2 : false
         }
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -24,25 +26,8 @@ export default class LoginRegister extends Component {
 
   render() {
     const style={
-        heading: {
-            fontSize: 30,
-            textAlign: 'center',
-            margin: 10
-        },
-        caption:{
-            margin:0,
-            paddingLeft:100,
-        },
-        wrapping:{
-            paddingTop: 100,
-        },
-        input: {
-            // margin: 10
-        },
         card: {
             paddingTop: this.state.loginRegisterToggle ? 50 : 0,
-            paddingLeft: 25,
-            paddingRight: 25,
             paddingBottom: this.state.loginRegisterToggle ? 50 : 25,
         },
         p: {
@@ -62,59 +47,96 @@ export default class LoginRegister extends Component {
         }
     }
 
-    return (
-      <Grid style={style.wrapping}>
-        <Grid.Row centered>
-            <Grid.Column width={5}>
-                <Card style={style.card} raised color='red' centered fluid>
-                    <Card.Content>
-                        <Card.Header><span style={style.heading}><p><Icon name="student" />Intern<b>EOD</b></p></span></Card.Header>
-                        <Card.Description>
-                            {
-                                this.state.loginRegisterToggle ?
-                                <Form>
-                                    <Form.Input style={style.input} type="text" name="username" iconPosition="left" icon="user" placeholder="Username" />
-                                    <Form.Input style={style.input} type="password" iconPosition="left" icon="key" placeholder="Password" />
-                                    <p style={style.p}>
-                                        <Button basic icon labelPosition="right" onClick={()=>{this.handleSubmit('login')}}>
-                                            <Icon style={{background:'none'}} name="send"/>
-                                            Login
-                                        </Button>
-                                    </p>
-                                    <p style={style.p}>New Intern? - <span style={style.signup} onClick={()=>{this.setState({loginRegisterToggle: false})}}>SignUp!</span></p>
-                                    <p style={style.forgetpassword}>Forgot Password?</p>
-                                </Form> 
-                                :
-                                <Form>
-                                    {
-                                        this.state.registerPage1 === true && this.state.registerPage2 === false ?
-                                        <span>
-                                            <Form.Input type="text" name="fullname" placeholder="Enter Full Name" icon="id card outline" iconPosition="left"/>
-                                            <Form.Input type="text" name="chooseusername" placeholder="Choose Username" icon="user" iconPosition="left" />
-                                            <Form.Input type="password" name="enterpassword" placeholder="Enter Password" icon="key" iconPosition="left" />
-                                            <Form.Input type="password" name="repeatpassword" placeholder="Repeat Password" icon="privacy" iconPosition="left" />
-                                            <p style={style.p}>
-                                            <Button onClick={()=>{this.setState({registerPage1: false, registerPage2: true})}} basic icon labelPosition="right">
-                                                <Icon style={{background: 'none'}} name="right arrow"/>
-                                                Next
-                                            </Button>
-                                            </p>
-                                        </span>
-                                        :
-                                        <span>
-                                            <Form.Input type="text" name="secretquestion" placeholder="Enter Secret Question" icon="question" iconPosition="left" />
-                                            <Form.Input type="text" name="secretanswer" placeholder="Enter Answer" icon="write" iconPosition="left"/>
-                                            <p style={style.p}>
-                                            <Button icon labelPosition="right" type="submit" onClick={()=>{this.handleSubmit('register')}} positive>
-                                                <Icon style={{background: 'none'}} name="checkmark"/>
-                                                Submit
-                                            </Button>
-                                            </p>
-                                        </span>
-                                    }  
-                                </Form>
+    var formComponents = ()=>{
+        switch(this.state.option){
+            case 'login':
+                return (
+                    <Form>
+                        <Form.Input type="text" name="username" iconPosition="left" icon="user" placeholder="Username" />
+                        <Form.Input type="password" name="password" iconPosition="left" icon="key" placeholder="Password" />
+                        <p style={style.p}>
+                            <Button basic icon labelPosition="right" onClick={()=>{this.handleSubmit('login')}}>
+                                <Icon style={{background:'none'}} name="send"/>
+                                Login
+                            </Button>
+                        </p>
+                        <p style={style.p}>New Intern? - <span style={style.signup} onClick={()=>{this.setState({option: 'register'})}}>SignUp!</span></p>
+                        <p style={style.forgetpassword} onClick={()=>{this.setState({option: 'forgotpassword'})}}>Forgot Password?</p>
+                    </Form>
+                );
+            
+            case 'register':
+                return (
+                    <Form>
+                        {
+                            this.state.registerPage1 === true && this.state.registerPage2 === false ?
+                            <span>
+                                <Form.Input type="text" name="fullname" placeholder="Enter Full Name" icon="id card outline" iconPosition="left"/>
+                                <Form.Input type="text" name="chooseusername" placeholder="Choose Username" icon="user" iconPosition="left" />
+                                <Form.Input type="password" name="enterpassword" placeholder="Enter Password" icon="key" iconPosition="left" />
+                                <Form.Input type="password" name="repeatpassword" placeholder="Repeat Password" icon="privacy" iconPosition="left" />
+                                <p style={style.p}>
+                                <Button onClick={()=>{this.setState({registerPage1: false, registerPage2: true})}} basic icon labelPosition="right">
+                                    <Icon style={{background: 'none'}} name="right arrow"/>
+                                    Next
+                                </Button>
+                                </p>
+                            </span>
+                            :
+                            <span>
+                                <Form.Input type="text" name="secretquestion" placeholder="Enter Secret Question" icon="question" iconPosition="left" />
+                                <Form.Input type="text" name="secretanswer" placeholder="Enter Answer" icon="write" iconPosition="left"/>
+                                <p style={style.p}>
+                                <Button onClick={()=>{this.setState({registerPage1: true, registerPage2: false})}} basic icon labelPosition="left">
+                                    <Icon style={{background: 'none'}} name="left arrow"/>
+                                    Back
+                                </Button>
+                                <Button icon labelPosition="right" type="submit" onClick={()=>{this.handleSubmit('register')}} positive>
+                                    <Icon style={{background: 'none'}} name="checkmark"/>
+                                    Submit
+                                </Button>
+                                </p>
+                            </span>
+                        } 
+                        <p style={style.p}>Already registered? - <span onClick={()=>{this.setState({option: 'login'})}} style={style.signup}>Login</span></p>
+                    </Form>
+                );
+            
+            case 'forgotpassword':
+                return (
+                    <Form>
+                        {this.state.forgotPage1 === true && this.state.forgotPage2 === false ?
+                            <span>
+                                <Form.Input type="text" name="forgotusername" placeholder="Enter Username" icon="user" iconPosition="left" />
+                                <Button icon labelPosition="right">
+                                    <Icon name="right arrow"/>
+                                    Next
+                                </Button>
+                            </span>
+                            :
+                            <span>
+                                <Message visible>What is your Date of Birth?</Message>
+                                <Form.Input type="text" name="secretanswer" placeholder="Enter Your Secret Answer" icon="key" iconPosition="left" />
+                                <Button icon labelPosition="right">
+                                    <Icon name="undo" />        
+                                    Reset
+                                </Button>
+                            </span>
+                        }
+                    </Form>
+                )
+        }
+    }
 
-                            }
+    return (
+      <Grid id="loginwrapping">
+        <Grid.Row centered>
+            <Grid.Column computer={5} mobile={16}>
+                <Card id="logincard" style={style.card} raised color='red' centered fluid>
+                    <Card.Content>
+                        <Card.Header><span id="loginheading"><p><Icon name="student" />Intern<b>EOD</b></p></span></Card.Header>
+                        <Card.Description>
+                            {formComponents()}
                         </Card.Description>
                     </Card.Content>
                 </Card>
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import {Provider} from "react-redux";
 import 'semantic-ui-css/semantic.min.css';
 import store from "./Store/store";
-
-
-
+import './component/css/style.css';
 
 
 ReactDOM.render( <Provider store={store}><AppRouter /></Provider>, document.getElementById('root'));
